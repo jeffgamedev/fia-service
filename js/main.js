7d490ccc99cd3839554baffdd3bfde2f6310ec0f
Jeffol <jeph.brooks@gmail.com>
2018-06-08 01:23:49 -0500

    Implement form saving/updating/submitting form data.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,55 @@ var config = {
   storageBucket: "fia-service.appspot.com",
   messagingSenderId: "173186090244"
 };
-firebase.initializeApp(config);
+
+var app = firebase.initializeApp(config);
+var database = app.database();
+var survey = undefined;
+
+var saveData = function() {
+
+  if (survey === undefined)
+  {
+    survey = database.ref("submissions").push({
+      date_now: new Date().toLocaleString(),
+    });
+  }
+
+  var updateData = {};
+  var inputs = $("input").each(function() {
+    var $this = $(this);
+    var inputName = $this.attr('name');
+    var inputVal = $this.val();
+    switch ($this.attr('type')) {
+      case 'radio':
+        if ($this.is(':checked')) {
+          updateData[inputName] = inputVal;
+        }
+      break;
+      case 'checkbox':
+        if ($this.prop('checked')) {
+          if (updateData[inputName]) {
+            updateData[inputName] += ", " + inputVal;
+          }
+          else {
+            updateData[inputName] = inputVal;
+          }
+        }
+        break;
+      default:
+        updateData[inputName] = inputVal;
+        break;
+    }
+  });
+  var selects = $("select option:selected").each(function() {
+    var $this = $(this);
+    var inputVal = $this.val();
+    var inputName = $this.parent().attr('name');
+    updateData[inputName] = inputVal;
+  });
+
+  database.ref("submissions/" + survey.key).update(updateData);
+};
 
 var formSubmission = function( event ) {
   event.preventDefault();
@@ -25,6 +73,7 @@ var hideAllForms = function() {
 };
 
 var formNext = function(nextSection) {
+  saveData();
   hideAllForms();
   var nextForm = $("#" + nextSection);
   nextForm.removeClass("d-none");
